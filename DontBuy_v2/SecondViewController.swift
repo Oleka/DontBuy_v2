26f27293e3f4181f350fc6cec2850eed3c303ee6
Oleka <olistudio.ru@gmail.com>
2016-10-17 10:08:37 +0300

    Add new Storyboard = in Portrait mode done

--- DontBuy_v2/SecondViewController.swift
+++ DontBuy_v2/SecondViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet var labelName   : UILabel?
+    @IBOutlet var labelReason : UILabel?
+    @IBOutlet var fieldName   : UITextField?
+    @IBOutlet var fieldReason  : UITextField?
+    @IBOutlet var buttonSave   : UIButton?
+    @IBOutlet var imgPhoto     : UIImageView?
+    @IBOutlet var imgCamera    : UIImageView?
+    @IBOutlet var agaric1      : UIButton?
+    @IBOutlet var agaric2      : UIButton?
+    @IBOutlet var agaric3      : UIButton?
+    @IBOutlet var agaric4      : UIButton?
+    @IBOutlet var agaric5      : UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,4 +35,33 @@ class SecondViewController: UIViewController {
 
 
 }
-
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
